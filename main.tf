496425eea4e9b29dc801360910853b926aff5efa
Stephen Attard <stephen.attard@opsguru.io>
2022-11-15 11:46:38 +0100

    Update, trigger

--- main.tf
+++ main.tf
@@ -1,8 +1,13 @@
-resource "aws_s3_bucket" "b" {
+resource "aws_s3_bucket" "this" {
   bucket = "my-tf-test-bucket-ops-guru-123"
 
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+  block_public_acls = true
 }
