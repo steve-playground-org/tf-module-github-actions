96b36f1e5bd49ac37d2e211fa6f73c74dca8c75b
Stephen Attard <stephen.attard@opsguru.io>
2022-11-15 11:56:08 +0100

    Update, trigger

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "aws_s3_bucket" "this" {
   bucket = "my-tf-test-bucket-ops-guru-123"
+  #tfsec:ignore:aws-s3-enable-bucket-encryption tfsec:ignore:aws-s3-encryption-customer-key tfsec:ignore:aws-s3-block-public-policy tfsec:ignore:aws-s3-ignore-public-acls tfsec:ignore:aws-s3-no-public-buckets tfsec:ignore:aws-s3-enable-bucket-logging tfsec:ignore:aws-s3-enable-versioning  tfsec:ignore:aws-s3-block-public-acls
 
   tags = {
     Name        = "My bucket"
@@ -7,7 +8,7 @@ resource "aws_s3_bucket" "this" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "this" {
-  bucket = aws_s3_bucket.this.id
-  block_public_acls = true
-}
+# resource "aws_s3_bucket_public_access_block" "this" {
+#   bucket = aws_s3_bucket.this.id
+#   block_public_acls = true
+# }
